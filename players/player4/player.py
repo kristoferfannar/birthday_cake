2023dfca937229bb6840eda1eacd92db471f09c3
Devanshi Guglani <dguglani25@students.claremontmckenna.edu>
2025-10-05 19:10:39 -0400

    Rollback some changes I made accidentally.

--- players/player4/player.py
+++ players/player4/player.py
@@ -8,5 +8,5 @@ class Player4(Player):
     def __init__(self, children: int, cake: Cake, cake_path: str | None) -> None:
         super().__init__(children, cake, cake_path)
 
-    def get_cuts(self) -> list[tuple[Point, Point]]: 
+    def get_cuts(self) -> list[tuple[Point, Point]]:
         return []
