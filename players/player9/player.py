aefbc4d070d3264dfdd497d20e175ec3ebf69f65
jparkhouse2024 <jparkhouse20@gmail.com>
2025-10-06 09:10:37 -0400

    Update player.py

--- players/player9/player.py
+++ players/player9/player.py
@@ -1,5 +1,5 @@
 from shapely import Point
-from players.player9.Weighted_Random import WeightedRandom
+# from players.player9.Weighted_Random import WeightedRandom
 from players.player9.crust_optimizing_player import CrustOptimizingPlayer
 
 from players.player import Player
@@ -10,8 +10,7 @@ class Player9(Player):
     def __init__(self, children: int, cake: Cake, cake_path: str | None) -> None:
         super().__init__(children, cake, cake_path)
         self.WR_player = CrustOptimizingPlayer(children, cake, cake_path)
-        #self.WR_player = WeightedRandom(children, cake, cake_path)
+        # self.WR_player = WeightedRandom(children, cake, cake_path)
 
     def get_cuts(self) -> list[tuple[Point, Point]]:
         return self.WR_player.get_cuts()
-    
