42730f83652511dee4b2d9995d50d29c552d1e49
Erin Jaen <erinljaen@gmail.com>
2025-10-13 18:38:43 -0400

    flower and update ratio threshold

--- players/player6/player.py
+++ players/player6/player.py
@@ -611,7 +611,7 @@ class Player6(Player):
         # If difference from target area < 0.125, treat it as equal → rely on ratio
         if area_score < 0.245:
             area_score = 0.0
-        if ratio_score <= 0.025:
+        if ratio_score <= 0.015:
             ratio_score = 0.0
 
         # adding line length as the last factor as after dividing area equally we would love to have more interior !
